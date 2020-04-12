7de798dee6cd57e8d02ba7a95c714ccf8800c8e2
ngirimana <chadrackngirimana@gmail.com>
2020-04-12 17:14:04 +0200

    Order Submission & Passing Data Between Pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,34 +92,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
-        // this.setState( { loading: true } );
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Max Schwarzmüller',
-        //         address: {
-        //             street: 'Teststreet 1',
-        //             zipCode: '41351',
-        //             country: 'Germany'
-        //         },
-        //         email: 'test@test.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post( '/orders.json', order )
-        //     .then( response => {
-        //         this.setState( { loading: false, purchasing: false } );
-        //     } )
-        //     .catch( error => {
-        //         this.setState( { loading: false, purchasing: false } );
-        //     } );
-        const querParams = [];
+        const queryParams = [];
         for (let i in this.state.ingredients) {
-            querParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[ i ]))
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[ i ]))
         }
-        const queryString = querParams.join('&')
+        queryParams.push('price='+ this.state.totalPrice.toFixed(2))
+        const queryString = queryParams.join('&')
         this.props.history.push({
             pathname: '/checkout',
             search: '?' + queryString
