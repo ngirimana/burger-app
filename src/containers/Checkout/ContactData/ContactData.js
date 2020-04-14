630d356d128846e48f08ee0af77257052000656e
ngirimana <chadrackngirimana@gmail.com>
2020-04-14 16:34:18 +0200

    Adding Custom Form Validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -13,7 +13,11 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'Your Name'
 				},
-				value: ''
+				value: '',
+				validation:{
+					required:true
+				},
+				valid:false
 			},
 			street: {
 				elementType: 'input',
@@ -21,7 +25,11 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'Your Street'
 				},
-				value: ''
+				value: '',
+				validation:{
+					required:true
+				},
+				valid:false
 			},
 			zipCode: {
 				elementType: 'input',
@@ -29,7 +37,13 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'Your ZIP Code'
 				},
-				value: ''
+				value: '',
+				validation:{
+					required:true,
+					minLength:5,
+					maxLength:5
+				},
+				valid:false
 			},
 			country: {
 				elementType: 'input',
@@ -37,7 +51,11 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'Your Country'
 				},
-				value: ''
+				value: '',
+				validation:{
+					required:true
+				},
+				valid:false
 			},
 			email: {
 				elementType: 'input',
@@ -45,7 +63,11 @@ class ContactData extends Component {
 					type: 'email',
 					placeholder: 'Your Mail'
 				},
-				value: ''
+				value: '',
+				validation:{
+					required:true
+				},
+				valid:false
 			},
 			deliveryMethod: {
 				elementType: 'select',
@@ -56,7 +78,11 @@ class ContactData extends Component {
 						{ value: 'firstclass', displayValue: 'First Class' }
 					],
 				},
-				value: ''
+				value: '',
+				validation:{
+					required:true
+				},
+				valid:false
 			}
 		},
 
@@ -85,6 +111,19 @@ class ContactData extends Component {
 				this.setState({ loading: false });
 			});
 	}
+	checkValidity=(value,rules)=>{
+		let isValid=false;
+		if(rules.required){
+			isValid=value.trim()!=='';
+		}
+		if(rules.minLength){
+			isValid=value.minLength>=rules.minLength
+		}
+		if(rules.maxLength){
+			isValid=value.maxLength>=rules.maxLength
+	ax
+		return isValid;
+	}
 	inputChangedHandler = (event, inputIdentifier) => {
 		const updateOrderForm = {
 			...this.state.orderForm
@@ -93,6 +132,7 @@ class ContactData extends Component {
 			...updateOrderForm[ inputIdentifier ]
 		}
 		updatedFormElement.value = event.target.value;
+		updatedFormElement.valid=this.checkValidity(updatedFormElement.value,updatedFormElement.validation)
 		updateOrderForm[inputIdentifier]=updatedFormElement
 		this.setState({orderForm:updateOrderForm})
 	}
