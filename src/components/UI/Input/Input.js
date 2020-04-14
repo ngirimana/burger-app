1bf13c7575bdd9b5633199e5a6410f5e2459df08
ngirimana <chadrackngirimana@gmail.com>
2020-04-14 21:08:59 +0200

    Working on an Error

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,11 +3,12 @@ import React from 'react';
 import classes from './Input.module.css';
 
 const input = ( props ) => {
-		let inputElement = null;
-		const inputClasses=[classes.InputElement];
-		if(props.invalid && props.shouldValidate && props.touched){
-			inputClasses.push(classes.Invalid)
-		}
+    let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
 
     switch ( props.elementType ) {
         case ( 'input' ):
