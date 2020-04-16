59a51343644d9ddb73eab7bafa658d0878af300b
ngirimana <chadrackngirimana@gmail.com>
2020-04-16 14:20:13 +0200

    Redux & UI State

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,8 +18,6 @@ class BurgerBuilder extends Component {
     //     this.state = {...}
     // }
     state = {
-        
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -44,7 +42,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({ purchasable: sum > 0 });
+       return sum > 0 ;
     }
     purchaseHandler = () => {
         this.setState({ purchasing: true });
@@ -59,7 +57,7 @@ class BurgerBuilder extends Component {
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[ i ]))
         }
-        queryParams.push('price=' + this.state.totalPrice.toFixed(2))
+        queryParams.push('price=' + this.props.price.toFixed(2))
         const queryString = queryParams.join('&')
         this.props.history.push({
             pathname: '/checkout',
@@ -85,7 +83,7 @@ class BurgerBuilder extends Component {
                          ingredientAdded={this.props.onIngredientAdded}
                          ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={ disabledInfo }
-                        purchasable={ this.state.purchasable }
+                        purchasable={ this.updatePurchaseState(this.props.ings) }
                         ordered={ this.purchaseHandler }
                         price={ this.props.price} />
                 </Auxiliary>
