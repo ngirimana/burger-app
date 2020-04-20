5c36346535f436f21a9b3f2d6bf44f1f2a393217
ngirimana <chadrackngirimana@gmail.com>
2020-04-20 07:31:38 +0200

    Combining Reducers

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -200,9 +200,9 @@ class ContactData extends Component {
 }
 const mapStateToProps = state => {
 	return {
-			ings: state.ingredients,
-			price:state.totalPrice,
-			loading:state.loading
+			ings: state.burgerBuilder.ingredients,
+			price:state.burgerBuilder.totalPrice,
+			loading:state.order.loading
 	}
 }
 const mapDispatchToProps= dispatch=>{
--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware,compose } from 'redux';
-import thunk from 'redux-thunk'
-
-
+import { createStore, applyMiddleware,compose,combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import ordersReducer from './store/reducers/order';
+
+const rootReducer=combineReducers({
+	burgerBuilder:burgerBuilderReducer,
+	order:ordersReducer
+})
 const composeEnhancers= window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
-const store = createStore(burgerBuilderReducer,composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
 	<Provider store={ store }>
