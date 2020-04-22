b5b394c3d4aaeddcae5a2f78f2054528bcca220f
ngirimana <chadrackngirimana@gmail.com>
2020-04-22 14:42:15 +0200

    Forwarding Unauthenticated Users

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,7 +38,11 @@ class BurgerBuilder extends Component {
        return sum > 0 ;
     }
     purchaseHandler = () => {
+        if(this.props.isAuthenticated){
         this.setState({ purchasing: true });
+        }else{
+            this.props.history.push('/auth')
+        }
     }
 
     purchaseCancelHandler = () => {
@@ -70,6 +74,7 @@ class BurgerBuilder extends Component {
                         disabled={ disabledInfo }
                         purchasable={ this.updatePurchaseState(this.props.ings) }
                         ordered={ this.purchaseHandler }
+                        isAuth={this.props.isAuthenticated}
                         price={ this.props.price} />
                 </Auxiliary>
             );
@@ -95,7 +100,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price:state.burgerBuilder.totalPrice,
-        error:state.burgerBuilder.error
+        error:state.burgerBuilder.error,
+        isAuthenticated:state.auth.token!==null
     }
 }
 const mapDispatchToProps = dispatch => {
