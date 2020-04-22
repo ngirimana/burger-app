b330702ac57a333220e8de50b0f147ce6d83ef80
ngirimana <chadrackngirimana@gmail.com>
2020-04-22 17:44:49 +0200

    Displaying User Specific Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {connect}from 'react-redux'
+import { connect } from 'react-redux'
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/WithErrorHandler/WithErrorHandler';
@@ -8,36 +8,38 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
     componentDidMount() {
-        this.props.onFetchOrders(this.props.token)
+        this.props.onFetchOrders(this.props.token, this.props.userId)
     }
 
-    render () {
-        let orders=<Spinner/>
-        if(!this.props.loading){
-            orders=this.props.orders.map(order => (
-                <Order 
-                    key={order.id}
-                    ingredients={order.ingredients}
-                    price={order.price} />
+    render() {
+        let orders = <Spinner />
+        if (!this.props.loading) {
+            orders = this.props.orders.map(order => (
+                <Order
+                    key={ order.id }
+                    ingredients={ order.ingredients }
+                    price={ order.price } />
             ))
         }
         return (
             <div>
-                {orders}
+                { orders }
             </div>
         );
     }
 }
-const mapStateToProps=state=>{
-    return{
-        orders:state.order.orders,
-        loading:state.order.loading,
-        token:state.auth.token
-    }}
-const mapDispacthToProps=dispatch=>{
-    return{
-onFetchOrders:(token)=>dispatch(actions.fetchOrders(token))
+const mapStateToProps = state => {
+    return {
+        orders: state.order.orders,
+        loading: state.order.loading,
+        token: state.auth.token,
+        userId: state.auth.userId
+    }
+}
+const mapDispacthToProps = dispatch => {
+    return {
+        onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
     }
 }
 
-export default connect(mapStateToProps,mapDispacthToProps) (withErrorHandler(Orders, axios));
+export default connect(mapStateToProps, mapDispacthToProps)(withErrorHandler(Orders, axios));
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,78 +1,80 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../axios-orders';
 
-export const purchaseBurgerSuccess=(id,orderData)=>{
-	return{
-		type:actionTypes.PURSCHASE_BURGER_SUCCESS,
-		orderId:id,
-		orderData:orderData
+export const purchaseBurgerSuccess = (id, orderData) => {
+	return {
+		type: actionTypes.PURSCHASE_BURGER_SUCCESS,
+		orderId: id,
+		orderData: orderData
 
 	}
 }
-export const purchaseBurgerFail=(error)=>{
-	return{
-		type:actionTypes.PURSCHASE_BURGER_FAIL,
-		error:error
+export const purchaseBurgerFail = (error) => {
+	return {
+		type: actionTypes.PURSCHASE_BURGER_FAIL,
+		error: error
 	}
 }
-export const purchaseBurgerStart=()=>{
-	return{
-		type:actionTypes.PURSCHASE_BURGER_START
+export const purchaseBurgerStart = () => {
+	return {
+		type: actionTypes.PURSCHASE_BURGER_START
 	}
 }
-export const purchaseBurger=(orderData,token)=>{
-	return dispatch=>{
+export const purchaseBurger = (orderData, token) => {
+	return dispatch => {
 		dispatch(purchaseBurgerStart())
-		axios.post('/orders.json?auth='+token, orderData)
-		.then(response => {
-		console.log(response.data)
-		dispatch(purchaseBurgerSuccess(response.data.name,orderData))
-		})
-		.catch(error => {
-		dispatch(purchaseBurgerFail(error))
-		});
+		axios.post('/orders.json?auth=' + token, orderData)
+			.then(response => {
+				console.log(response.data)
+				dispatch(purchaseBurgerSuccess(response.data.name, orderData))
+			})
+			.catch(error => {
+				dispatch(purchaseBurgerFail(error))
+			});
 	}
 }
-export const purchaseInit=()=>{
-	return{
-		type:actionTypes.PURSCHASE_INIT
+export const purchaseInit = () => {
+	return {
+		type: actionTypes.PURSCHASE_INIT
 	}
 }
-export const fetchOrderSuccess=(orders)=>{
-	return{
-		type:actionTypes.FETCH_ORDER_SUCCESS,
-		orders:orders
-	
+export const fetchOrderSuccess = (orders) => {
+	return {
+		type: actionTypes.FETCH_ORDER_SUCCESS,
+		orders: orders
+
 	}
 }
-export const fetchOrdersFail=(error)=>{
-	return{
-		type:actionTypes.FETCH_ORDER_FAIL,
-		error:error
+export const fetchOrdersFail = (error) => {
+	return {
+		type: actionTypes.FETCH_ORDER_FAIL,
+		error: error
 	}
 }
-export const fetchOrdersStart=()=>{
-	return{
-		type:actionTypes.FETCH_ORDER_START
+export const fetchOrdersStart = () => {
+	return {
+		type: actionTypes.FETCH_ORDER_START
 	}
 }
-export const fetchOrders=(token)=>{
-	return dispatch=>{
+export const fetchOrders = (token, userId) => {
+	return dispatch => {
 		dispatch(fetchOrdersStart())
-		axios.get('/orders.json?auth='+token)
-            .then(res => {
-                const fetchedOrders = [];
-                for (let key in res.data) {
-                    fetchedOrders.push({
-                        ...res.data[key],
-                        id: key
-                    });
-								}
-								dispatch(fetchOrderSuccess(fetchedOrders))
-                
-            })
-            .catch(err => {
-                dispatch(fetchOrdersFail(err))
-            });
+		const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+		axios.get( '/orders.json' + queryParams)
+			.then(res => {
+				const fetchedOrders = [];
+				for (let key in res.data) {
+					fetchedOrders.push({
+						...res.data[ key ],
+						id: key
+					});
+				}
+				dispatch(fetchOrderSuccess(fetchedOrders))
+
+			})
+			.catch(err => {
+				console.log(err)
+				dispatch(fetchOrdersFail(err))
+			});
 	}
 }
