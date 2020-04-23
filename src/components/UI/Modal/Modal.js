1d28ccc605fb4b4067d29837adc9b19fd8f2dfac
ngirimana <chadrackngirimana@gmail.com>
2020-04-23 12:41:20 +0200

    remove console log()

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,11 +9,6 @@ class Modal extends Component {
     shouldComponentUpdate ( nextProps, nextState ) {
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
-
-    componentWillUpdate () {
-        console.log('[Modal] WillUpdate');
-    }
-
     render () {
         return (
             <Auxiliary>
