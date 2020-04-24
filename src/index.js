72c22d8275f5da81f1feb19cd27dc1cc6f13e32c
ngirimana <chadrackngirimana@gmail.com>
2020-04-24 15:57:50 +0200

    deploy using firebase

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={"/burger"}>
             <App />
         </BrowserRouter>
     </Provider>
