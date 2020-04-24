7db70a878403d426ba70547dcabb248f0b623a65
ngirimana <chadrackngirimana@gmail.com>
2020-04-24 13:56:25 +0200

    test component with props using contains method

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -20,5 +20,9 @@ describe('<NavigationItems/>', () => {
 		wrapper.setProps({isAuthenticated:true})
 		expect(wrapper.find(NavigationItem)).toHaveLength(3)
 	})
+	it('should render three <navigationItems element  if  authenticated', () => {
+		wrapper.setProps({isAuthenticated:true})
+		expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true)
+	})
 
 })
